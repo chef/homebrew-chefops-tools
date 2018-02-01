ad2489a407049e28958647570a5e8c5ee493ef05
Mark Harrison <mark@mivok.net>
2018-02-01 12:33:00 -0500

    Update aws-mfa formula

--- Formula/aws-mfa.rb
+++ Formula/aws-mfa.rb
@@ -4,52 +4,47 @@ class AwsMfa < Formula
 
   desc "AWS MFA cli tool"
   homepage "https://github.com/broamski/aws-mfa"
-  url "https://files.pythonhosted.org/packages/9b/5c/4ea7e0cf9bde32c0d56301d515593cad3d3d522b51e5a712dd880f335410/aws-mfa-0.0.8.tar.gz"
-  sha256 "46e7e5c7e72300ab8d071ce10dbf89875374120ff77ee074b56f901b4df228dd"
+  url "https://files.pythonhosted.org/packages/83/88/5c71bbd7b2a29548488e38a2e48023d62f06a264254c2b127414b9f3d000/aws-mfa-0.0.10.tar.gz"
+  sha256 "16f41dc54c9f97982536800eb4c5dbb0933a00f631f84637170e2427bf720f5c"
 
-  depends_on :python
+  depends_on "python3"
 
   resource "boto3" do
-    url "https://files.pythonhosted.org/packages/58/61/50d2e459049c5dbc963473a71fae928ac0e58ffe3fe7afd24c817ee210b9/boto3-1.4.4.tar.gz"
-    sha256 "518f724c4758e5a5bed114fbcbd1cf470a15306d416ff421a025b76f1d390939"
+    url "https://files.pythonhosted.org/packages/17/12/d30d2efbd0f88ef279c65530a494a5c108b9b37aff5e992a805c36211fe5/boto3-1.5.22.tar.gz"
+    sha256 "5430b5cd532fe56ccc9eaf1ed433ac74805811b931ae1e44eb896af98a1297f0"
   end
 
   resource "botocore" do
-    url "https://files.pythonhosted.org/packages/7d/03/91cc754901d8a31a127931cfed50295f3495b6c1043fe8fe89d1f8aec8a7/botocore-1.5.31.tar.gz"
-    sha256 "9a11fa93785722822f291648920964724c41f756593dbf9a2d22ff2ea6a8b04f"
+    url "https://files.pythonhosted.org/packages/f8/f0/f033c278c09ff5a0e858581c597e1b9989f55e8f108dc1de8534422f47fc/botocore-1.8.36.tar.gz"
+    sha256 "b2c9e0fd6d14910f759a33c19f8315dddedbb3c5569472b7be7ceed4f001a675"
   end
 
   resource "docutils" do
-    url "https://files.pythonhosted.org/packages/37/38/ceda70135b9144d84884ae2fc5886c6baac4edea39550f28bcd144c1234d/docutils-0.12.tar.gz"
-    sha256 "c7db717810ab6965f66c8cf0398a98c9d8df982da39b4cd7f162911eb89596fa"
-  end
-
-  resource "futures" do
-    url "https://files.pythonhosted.org/packages/c0/12/927b89a24dcb336e5af18a8fbf581581c36e9620ae963a693a2522b2d340/futures-2.2.0.tar.gz"
-    sha256 "151c057173474a3a40f897165951c0e33ad04f37de65b6de547ddef107fd0ed3"
+    url "https://files.pythonhosted.org/packages/84/f4/5771e41fdf52aabebbadecc9381d11dea0fa34e4759b4071244fa094804c/docutils-0.14.tar.gz"
+    sha256 "51e64ef2ebfb29cae1faa133b3710143496eca21c530f3f71424d77687764274"
   end
 
   resource "jmespath" do
-    url "https://files.pythonhosted.org/packages/c7/b6/4f25dc12e08fef1493c5bb6254342e609961eb01cbbc6065ca10b4f63e1b/jmespath-0.8.0.tar.gz"
-    sha256 "2c3d78a257e831b7d2855e2d00a24c2110f685ae31215d62cac5524d6108dc7a"
+    url "https://files.pythonhosted.org/packages/e5/21/795b7549397735e911b032f255cff5fb0de58f96da794274660bca4f58ef/jmespath-0.9.3.tar.gz"
+    sha256 "6a81d4c9aa62caf061cb517b4d9ad1dd300374cd4706997aff9cd6aedd61fc64"
   end
 
   resource "python-dateutil" do
-    url "https://files.pythonhosted.org/packages/b6/ff/5eaa688dd8ce78913f47438f9b40071a560126ac3e95f9b9be27dfe546a7/python-dateutil-2.4.2.tar.gz"
-    sha256 "3e95445c1db500a344079a47b171c45ef18f57d188dffdb0e4165c71bea8eb3d"
+    url "https://files.pythonhosted.org/packages/54/bb/f1db86504f7a49e1d9b9301531181b00a1c7325dc85a29160ee3eaa73a54/python-dateutil-2.6.1.tar.gz"
+    sha256 "891c38b2a02f5bb1be3e4793866c8df49c7d19baabf9c1bad62547e0b4866aca"
   end
 
   resource "s3transfer" do
-    url "https://files.pythonhosted.org/packages/8b/13/517e8ec7c13f0bb002be33fbf53c4e3198c55bb03148827d72064426fe6e/s3transfer-0.1.10.tar.gz"
-    sha256 "ba1a9104939b7c0331dc4dd234d79afeed8b66edce77bbeeecd4f56de74a0fc1"
+    url "https://files.pythonhosted.org/packages/b1/a6/24d960ee5f21eb2f9e2e938be44b9929bf9f85a570b9582c50c14e7c7ec7/s3transfer-0.1.12.tar.gz"
+    sha256 "10891b246296e0049071d56c32953af05cea614dca425a601e4c0be35990121e"
   end
 
   resource "six" do
-    url "https://files.pythonhosted.org/packages/16/64/1dc5e5976b17466fd7d712e59cbe9fb1e18bec153109e5ba3ed6c9102f1a/six-1.9.0.tar.gz"
-    sha256 "e24052411fc4fbd1f672635537c3fc2330d9481b18c0317695b46259512c91d5"
+    url "https://files.pythonhosted.org/packages/16/d8/bc6316cf98419719bd59c91742194c111b6f2e85abac88e496adefaf7afe/six-1.11.0.tar.gz"
+    sha256 "70e8a77beed4562e7f14fe23a786b54f6296e34344c23bc42f07b15018ff98e9"
   end
-
   def install
+    virtualenv_create(libexec, "python3")
     virtualenv_install_with_resources
   end
 
